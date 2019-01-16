053fa9feb4b2d5d45504116f08538adb3e5184f1
Yury.Kakareka <Yury.Kakareka@WS-025-41>
2019-01-16 16:24:56 +0300

    feat added other constructors for custom exceptions

--- Collector.BL/Exceptions/NoPermissionException.cs
+++ Collector.BL/Exceptions/NoPermissionException.cs
@@ -4,8 +4,14 @@ namespace Collector.BL.Exceptions
 {
     public class NoPermissionException : Exception
     {
+        public NoPermissionException()
+        {
+        }
         public NoPermissionException(string message)
             : base(message)
         { }
+        public NoPermissionException(string message, Exception inner) : base(message, inner)
+        {
+        }
     }
 }
--- Collector.BL/Exceptions/ServerFailException.cs
+++ Collector.BL/Exceptions/ServerFailException.cs
@@ -4,8 +4,14 @@ namespace Collector.BL.Exceptions
 {
     public class ServerFailException : Exception
     {
+        public ServerFailException()
+        {
+        }
         public ServerFailException(string message)
             : base(message)
         { }
+        public ServerFailException(string message, Exception inner) : base(message, inner)
+        {
+        }
     }
 }
