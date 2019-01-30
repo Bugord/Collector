6c7666d07bc45ea2892c16821c132af1ff976134
Yury.Kakareka <Yury.Kakareka@WS-025-41>
2019-01-30 14:19:25 +0300

    feat added migrations

--- Collector.DAO/Migrations/20190125114654_addedAttributesMigration.Designer.cs
+++ Collector.DAO/Migrations/20190125114654_addedAttributesMigration.Designer.cs
@@ -4,14 +4,16 @@ using Collector.DAO.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Collector.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190125114654_addedAttributesMigration")]
+    partial class addedAttributesMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Collector.DAO/Migrations/20190125114654_addedAttributesMigration.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Collector.Migrations
+{
+    public partial class addedAttributesMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatMessages_Users_AuthorId",
+                table: "ChatMessages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackMessages_Users_AuthorId",
+                table: "FeedbackMessages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackMessages_Feedbacks_FeedbackId",
+                table: "FeedbackMessages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FieldName",
+                table: "FieldChanges",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ChangeId",
+                table: "FieldChanges",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FeedbackId",
+                table: "FeedbackMessages",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AuthorId",
+                table: "FeedbackMessages",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AuthorId",
+                table: "ChatMessages",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ChangedDebtId",
+                table: "Changes",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatMessages_Users_AuthorId",
+                table: "ChatMessages",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackMessages_Users_AuthorId",
+                table: "FeedbackMessages",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackMessages_Feedbacks_FeedbackId",
+                table: "FeedbackMessages",
+                column: "FeedbackId",
+                principalTable: "Feedbacks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChatMessages_Users_AuthorId",
+                table: "ChatMessages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackMessages_Users_AuthorId",
+                table: "FeedbackMessages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FeedbackMessages_Feedbacks_FeedbackId",
+                table: "FeedbackMessages");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FieldName",
+                table: "FieldChanges",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ChangeId",
+                table: "FieldChanges",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "FeedbackId",
+                table: "FeedbackMessages",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AuthorId",
+                table: "FeedbackMessages",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "AuthorId",
+                table: "ChatMessages",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "ChangedDebtId",
+                table: "Changes",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChatMessages_Users_AuthorId",
+                table: "ChatMessages",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackMessages_Users_AuthorId",
+                table: "FeedbackMessages",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FeedbackMessages_Feedbacks_FeedbackId",
+                table: "FeedbackMessages",
+                column: "FeedbackId",
+                principalTable: "Feedbacks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
