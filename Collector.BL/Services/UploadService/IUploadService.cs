df9bcdc93989886331623fee50e4f83cb4b6f918
Yury.Kakareka <Yury.Kakareka@WS-025-41>
2019-01-30 13:51:52 +0300

    feat added upload service

---
+++ Collector.BL/Services/UploadService/IUploadService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Collector.DAO.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Collector.BL.Services.UploadService
+{
+    public interface IUploadService
+    {
+        Task<string> UploadFileAsync(IFormFile file, UploadType uploadType);
+    }
+}
---
+++ Collector.DAO/Entities/Upload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Collector.DAO.Entities
+{
+    public class Upload: BaseEntity
+    {
+        [StringLength(500)]
+        [Required]
+        public string Name { get; set; }
+        [StringLength(500)]
+        [Required]
+        public string OriginalName { get; set; }
+        [StringLength(500)]
+        [Required]
+        public string Extention { get; set; }
+        [Required]
+        public long Size { get; set; }
+        [StringLength(500)]
+        [Required]
+        public string Path { get; set; }
+        [Required]
+        public UploadType Type { get; set; }
+    }
+
+    public enum UploadType
+    {
+        Avatar,
+        Chat
+    }
+}
