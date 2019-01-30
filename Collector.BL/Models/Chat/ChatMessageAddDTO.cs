51d3f55f9178c61d3237b98f74629164551a3f6b
Yury.Kakareka <Yury.Kakareka@WS-025-41>
2019-01-30 14:18:22 +0300

    feat added dto for chat message upload

---
+++ Collector.BL/Models/Chat/ChatMessageAddDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Collector.BL.Models.Chat
+{
+    public class ChatMessageAddDTO
+    {
+        public string SendToUsername { get; set; }
+        [Required]
+        [StringLength(500, MinimumLength = 1, ErrorMessage = "Chat message text length must be between 1 and 500")]
+        public string MessageText { get; set; }
+    }
+}
