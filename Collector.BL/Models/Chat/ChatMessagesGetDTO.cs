f724bb7b4b66414c057013e3667115ce04065abd
Yury.Kakareka <Yury.Kakareka@WS-025-41>
2019-01-30 13:56:43 +0300

    style files names changed

--- Collector.BL/Models/Chat/ChatMessagesGetDTO.cs
+++ Collector.BL/Models/Chat/ChatMessagesGetDTO.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Collector.BL.Models.Chat
 {
-    public class ChatGetMessagesDTO
+    public class ChatMessagesGetDTO
     {
         public string ChatWithUsername { get; set; }
         public int SkipMessages { get; set; }
