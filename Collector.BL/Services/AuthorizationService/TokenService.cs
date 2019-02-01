2a36dc64796a707ca9737276b100ed4311677a7e
Yury.Kakareka <Yury.Kakareka@WS-025-41>
2019-02-01 15:42:30 +0300

    fix expire time of token counted from UTC

--- Collector.BL/Services/AuthorizationService/TokenService.cs
+++ Collector.BL/Services/AuthorizationService/TokenService.cs
@@ -104,7 +104,7 @@ namespace Collector.BL.Services.AuthorizationService
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtKey"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
+            var expires = DateTime.UtcNow.AddDays(Convert.ToDouble(_configuration["JwtExpireDays"]));
             var token = new JwtSecurityToken(
                 _configuration["JwtIssuer"],
                 _configuration["JwtAudience"],
